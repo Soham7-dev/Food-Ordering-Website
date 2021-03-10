d565d096eb1982f2a87da6758d22041777ebd94d
Soham Das <64075511+Soham7-dev@users.noreply.github.com>
2021-03-10 12:33:37 +0530

    Delete JS directory

--- JS/navbar.js
+++
@@ -1,7 +0,0 @@
-const a = document.querySelector(".hamburger");
-const b = document.querySelector(".nav-links");
-const c = document.querySelector("p");
-
-a.addEventListener("click", () => {
-  b.classList.toggle("open");
-});
--- JS/sample.js
+++
@@ -1,7 +0,0 @@
-let btn = document.getElementById("food-btn");
-let overlay = document.getElementById("overlay");
-
-btn.addEventListener("click", () => {
-  overlay.style.display = "grid";
-  overlay.classList.add("animate-overlay");
-});
